3576aeb072549fab5f7663814e6df50525ea9012
syktyr21-maker <syktyr21@gmail.com>
2025-12-29 18:26:31 +0330

    Update send-ip.js

--- api/send-ip.js
+++ api/send-ip.js
@@ -1,1 +1,23 @@
+export default async function handler(req, res) {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+
+  // توکن و chat_id مستقیم جاسازی شده
+  const token = "7961668268:AAEGMLMj5TojYl3giXl_C8S0O2zH3Q8IeEU";
+  const chatId = "7198165253";
+
+  try {
+    await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `Visitor IP: ${ip}`
+      })
+    });
+    res.status(200).send("IP sent to Telegram!");
+  } catch (err) {
+    console.error("Telegram error:", err);
+    res.status(500).send("Error sending IP");
+  }
+}
 
